853c04215b5076bdf81288958392ad744b212efa
Ghada <ghada-alshabanat@hotmail.com>
2024-10-27 19:12:20 +0300

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Ghada Alshabanat on 24/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
